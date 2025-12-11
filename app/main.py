a9a6d5c02e114a1d505d597a2fe72396d41871d6
bonxia <dowhatidream@gmail.com>
2025-12-12 01:30:15 +0900

    feat: 임차인 추천 요청 API 구현 및 MQ 발행 로직 연동
    feat: 임차인 추천 요청 API 구현 및 MQ 발행 로직 연동

--- app/main.py
+++ app/main.py
@@ -2,11 +2,27 @@
 from modules.user.adapter.input.web.router.user_router import register_user_handler
 from modules.user.adapter.output.in_memory_user_repository import InMemoryUserRepository
 from modules.user.application.usecase.register_user import RegisterUserService
-from shared.infrastructure.config.settings import load_settings
+from shared.infrastructure.config.settings import Settings
+
+import os
+from fastapi import FastAPI
+from modules.pipeline.adapter.input.web.router.pipeline_router import router as pipeline_router
+from modules.pipeline.adapter.input.web.router.tenant_recommend_router import router as tenant_recommend_router
+from shared.infrastructure.db.connection import init_pg_pool
+app = FastAPI()
+
+app.include_router(pipeline_router, prefix="/pipeline")
+
+app.include_router(tenant_recommend_router)
+
+@app.on_event("startup")
+def startup_event():
+    init_pg_pool()
+    print("PostgreSQL pool initialized")
 
 
 def bootstrap():
-    settings = load_settings()
+    settings = Settings()
     user_repository = InMemoryUserRepository()
     register_user_usecase = RegisterUserService(user_repository)
     container = {
@@ -26,4 +42,10 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    import uvicorn
+
+    host = os.getenv("APP_HOST")
+    port = int(os.getenv("APP_PORT"))
+    uvicorn.run(app, host=host, port=port)
+
     main()
--- shared/infrastructure/config/settings.py
+++ shared/infrastructure/config/settings.py
@@ -1,11 +1,23 @@
 """Infrastructure settings placeholder (e.g., env parsing)."""
 import os
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 
+load_dotenv()
 
-class Settings:
-    def __init__(self):
-        self.env = os.environ.get("APP_ENV", "local")
+class Settings(BaseSettings):
+    PG_HOST: str = os.getenv("PG_HOST")
+    PG_PORT: str = os.getenv("PG_PORT")
+    PG_USER: str = os.getenv("PG_USER")
+    PG_PASSWORD: str = os.getenv("PG_PASSWORD")
+    PG_DATABASE: str = os.getenv("PG_DATABASE")
 
+settings = Settings()
 
-def load_settings() -> Settings:
-    return Settings()
+# class Settings:
+#     def __init__(self):
+#         self.env = os.environ.get("APP_ENV", "local")
+#
+#
+# def load_settings() -> Settings:
+#     return Settings()
